551ff71a41a356b237762b010598abccb05ae9fa
vikash baghel <vikashbaghel92@gmail.com>
2022-11-05 19:05:17 +0530

    function printAllVariables()

--- java day14/script.js
+++ java day14/script.js
@@ -1,7 +1,13 @@
 let myAge=22
-
-myAge--
-myAge--
-const myName="vikashbaghel"
-
-console.log(myAge,myName)
+let myName="vikas"
+let myFriendAge=25
+let sum=myAge+myFriendAge
+function printAllVariable(){
+console.log('My Age',myAge)
+console.log('My Name',myName)
+console.log('My Friend Age',myFriendAge)
+console.log('Sum of Age',sum)
+}
+printAllVariable()
+printAllVariable()
+printAllVariable()
