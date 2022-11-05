0aa6f2c42ac33e641b12a7a1a6de015bb90208e3
vikash baghel <vikashbaghel92@gmail.com>
2022-11-05 10:48:01 +0530

    java someNumberTwoThePower code

---
+++ java day 15/script.js
@@ -0,0 +1,9 @@
+ const two = 2
+// write the correct expression below
+const twoToThePower5 = two ** 5 
+
+const someNumber = 329826
+// write the correct expression here
+const someNumberToThePowerOf5 = someNumber ** 5
+
+console.log(someNumberToThePowerOf5)
