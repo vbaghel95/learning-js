7415b6562d6ad68c61f9f78ae2a1c3764392fb94
vikash baghel <vikashbaghel92@gmail.com>
2022-11-08 20:10:11 +0530

    FLEX BOX ADDED

--- java day 16/script.js
+++ java day 16/script.js
@@ -1,10 +1,7 @@
-function shouldIGetAlicense(age) {
-    if (age > 18) {
-        console.log('This person is Qualified')
-        return
-    }
-    else{
-        console.log('This person not Qualified')
-    }
-}
-shouldIGetAlicense(11)
+let myAge = 25
+let myFriendAge = 26
+
+function add (myAge, myFriendAge)
+return (myAge + myFriendAge)
+add()
+console.log(add)
