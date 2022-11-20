fc99075d9ccb9273947907388e029b2deedd9925
vikash baghel <vikashbaghel92@gmail.com>
2022-11-20 12:29:06 +0530

    auto ul li in java

--- java d18/script.js
+++ java d18/script.js
@@ -1,14 +1,20 @@
 const incrementBtn = document.querySelector('#increment')
 const decrementBtn = document.querySelector('#decrement')
+ const counterEl = document.getElementById('counter')
+
+ const ulElement = document.getElementById('list-items')
 let counter = 0
 
 function incrementCounter(){
-    const counterEl = document.getElementById('counter')
     counter++
     counterEl.innerText = counter
+
+    const li = document.createElement('li')
+    li.innerHTML = ' <b>Point</b> ' + counter
+
+    ulElement.appendChild(li)
 }
 function decrementCounter(){
-    const counterEl = document.getElementById('counter')
     counter--
     counterEl.innerText = counter
     
