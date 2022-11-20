6e1c5aeaef5d0241d905dcc5e6a9ed9b2c4f87fc
vikash baghel <vikashbaghel92@gmail.com>
2022-11-20 11:35:04 +0530

    increment and decrement counter app

--- java d18/script.js
+++ java d18/script.js
@@ -1,10 +1,17 @@
 const incrementBtn = document.querySelector('#increment')
+const decrementBtn = document.querySelector('#decrement')
 let counter = 0
 
 function incrementCounter(){
     const counterEl = document.getElementById('counter')
     counter++
     counterEl.innerText = counter
+}
+function decrementCounter(){
+    const counterEl = document.getElementById('counter')
+    counter--
+    counterEl.innerText = counter
     
 }
 incrementBtn.addEventListener('click', incrementCounter)
+decrementBtn.addEventListener('click', decrementCounter)
